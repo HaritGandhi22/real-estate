0eef0486a0fa249422afda5718662008227dbb0d
Harit Gandhi <harit.g@zillionica.com>
2024-02-08 18:43:05 +0530

    made header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,19 +5,21 @@ import About from './pages/About'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/about' element={<About/>}></Route>
-      <Route path='/sign-in' element={<SignIn/>}></Route>
-      <Route path='/sign-up' element={<SignUp/>}></Route>
-      <Route path='/profile' element={<Profile/>}></Route>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/sign-in' element={<SignIn />}></Route>
+          <Route path='/sign-up' element={<SignUp />}></Route>
+          <Route path='/profile' element={<Profile />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
